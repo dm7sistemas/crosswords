8170d16f2a89c7400bbe1104da677a4e5777b0bb
Mark Alldritt <alldritt@latenightsw.com>
2014-02-14 11:42:07 -0800

    Improve comments a little and add caching of the calculated values.

--- Crosswords/PuzzleClue.m
+++ Crosswords/PuzzleClue.m
@@ -12,6 +12,9 @@
 
 @implementation PuzzleClue
 
+@synthesize intersectingClues = mIntersectingClues;
+@synthesize length = mLength;
+
 - (instancetype)initWithPuzzle:(PuzzleHelper*) puzzle
                            row:(NSUInteger) row
                         column:(NSUInteger) column
@@ -39,59 +42,67 @@
 }
 
 - (NSUInteger) length {
-    //  Note that the answer may be longer for "trick" puzzles where a grid cell can
-    //  hold more than one letter.  This method needs to be made aware of this and
-    //  return an accurate number of grid cells for the clue.
+    //  Note that the answer may be longer for "trick" puzzles where a grid cell can hold more than one letter.  This method
+    //  addresses this issue and returns a length in Grid Cells even when an answer string is longer.
 
-    NSArray* grid = self.puzzle.puzzle[@"grid"];
-    NSUInteger rows = self.puzzle.rows;
-    NSUInteger columns = self.puzzle.columns;
-    NSUInteger row = self.row;
-    NSUInteger column = self.column;
-    NSUInteger result = 0;
-    NSUInteger answerLength = self.answer.length;
-    NSUInteger i = 0;
-    
-    while (i < answerLength) {
-        NSAssert2(row < rows, @"row (%d) too large (%d)", (int)row, (int)rows);
-        NSAssert2(column < columns, @"column (%d) too large (%d)", (int)column, (int)columns);
-        NSString* s = grid[columns * row + column];
-        NSAssert3([s isEqualToString:@" "] || [[self.answer substringWithRange:NSMakeRange(i, s.length)] isEqualToString:s], @"Answer '%@' does not match grid ('%@' != '%@')", self.answer, [self.answer substringWithRange:NSMakeRange(i, s.length)], s);
+    //  Cache this value on demand...
+    if (mLength == 0) {
+        NSArray* grid = self.puzzle.puzzle[@"grid"];
+        NSUInteger rows = self.puzzle.rows;
+        NSUInteger columns = self.puzzle.columns;
+        NSUInteger row = self.row;
+        NSUInteger column = self.column;
+        NSUInteger result = 0;
+        NSUInteger answerLength = self.answer.length;
+        NSUInteger i = 0;
         
-        i += s.length;
-        result += 1;
-        if (self.across)
-            ++column;
-        else
-            ++row;
+        while (i < answerLength) {
+            NSAssert2(row < rows, @"row (%d) too large (%d)", (int)row, (int)rows);
+            NSAssert2(column < columns, @"column (%d) too large (%d)", (int)column, (int)columns);
+            NSString* s = grid[columns * row + column];
+            NSAssert3([s isEqualToString:@" "] || [[self.answer substringWithRange:NSMakeRange(i, s.length)] isEqualToString:s], @"Answer '%@' does not match grid ('%@' != '%@')", self.answer, [self.answer substringWithRange:NSMakeRange(i, s.length)], s);
+            
+            i += s.length;
+            result += 1;
+            if (self.across)
+                ++column;
+            else
+                ++row;
+        }
+        mLength = result;
     }
-    return result;
+    return mLength;
 }
 
 - (NSArray*) intersectingClues {
     //  Determine all the clues that intersect with the answer for a clue.  This is brute force, but for now, its good enough.
     
-    CGRect clueArea = self.area;
-    NSMutableArray* result = nil;
-    
-    for (PuzzleClue* aClue in self.puzzle.cluesAcross.allValues) {
-        if (![aClue isEqual:self] && CGRectIntersectsRect(clueArea, aClue.area)) {
-            if (result)
-                [result addObject:aClue];
-            else
-                result = [NSMutableArray arrayWithObject:aClue];
+    //  Cache this value on demand...
+    if (!mIntersectingClues) {
+        CGRect clueArea = self.area;
+        NSMutableArray* result = nil;
+        
+        for (PuzzleClue* aClue in self.puzzle.cluesAcross.allValues) {
+            if (![aClue isEqual:self] && CGRectIntersectsRect(clueArea, aClue.area)) {
+                if (result)
+                    [result addObject:aClue];
+                else
+                    result = [NSMutableArray arrayWithObject:aClue];
+            }
         }
-    }
-    for (PuzzleClue* aClue in self.puzzle.cluesDown.allValues) {
-        if (![aClue isEqual:self] && CGRectIntersectsRect(clueArea, aClue.area)) {
-            if (result)
-                [result addObject:aClue];
-            else
-                result = [NSMutableArray arrayWithObject:aClue];
+        for (PuzzleClue* aClue in self.puzzle.cluesDown.allValues) {
+            if (![aClue isEqual:self] && CGRectIntersectsRect(clueArea, aClue.area)) {
+                if (result)
+                    [result addObject:aClue];
+                else
+                    result = [NSMutableArray arrayWithObject:aClue];
+            }
         }
+        
+        mIntersectingClues = result.copy; // non-mutable version is cached
     }
     
-    return result.copy; // return a non-mutable version...
+    return mIntersectingClues;
 }
 
 @end
